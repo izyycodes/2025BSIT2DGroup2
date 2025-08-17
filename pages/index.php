280150978f7cf005c783b813952a04f2dce4a12d
izyycodes <s2100406@usls.edu.ph>
2025-08-17 23:09:09 +0800

    Add files via upload

---
+++ pages/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BantayBaha</title>
+    <link rel="icon" type="image/ico" href="../images/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<body>
+
+    <?php require "../views/index_navbar.php" ?>
+
+    <section class="hero-container">
+    
+        <div class="hero-text">
+            <div class="bantay-baha">
+                <p>Bantay</p>
+                <p style="text-align: center;">Baha</p>
+            </div>
+
+            <div class="slash">
+                <span>/</span>
+            </div>
+
+            <div class="tagline">
+                <p style="text-align: center;">Ligtas ang Barangay,</p>
+                <p style="text-align: center;"> Alerto ang Mamamayan.</p>
+            </div>
+        </div>
+
+        <div class="hero-button">
+            <button class="get-started-button">
+                <a href="../pages/sign_up.php">Get Started</a>
+            </button>
+        </div>
+    </section>
+
+    <section class="features">
+        <h3>Features</h3>
+        
+        <div class="features-container">
+            <!-- River Monitoring -->
+            <div class="features-card">
+                <div class="features-title">
+                    <img width="35" height="35" src="../icons/river_monitoring.png" alt="River Monitoring">
+                    <h4>River Monitoring</h4>
+                </div>
+
+                <p>View real-time updates on river water levels to track potential flooding in your area.</p>
+            </div>
+            
+            <!-- Community Reports -->
+            <div class="features-card">
+                <div class="features-title">
+                    <img width="35" height="35" src="../icons/community_reports.png" alt="Community Reports">
+                    <h4>Community Reports</h4>
+                </div>
+                
+                <p>Residents can report floods, blocked roads, or hazards with photos and brief descriptions.</p>
+                
+            </div>
+
+            <!-- Flood Alerts -->
+            <div class="features-card">
+                <div class="features-title">
+                    <img width="35" height="35" src="../icons/flood_alerts.png" alt=""Flood Alerts">
+                    <h4>Flood Alerts</h4>
+                </div>
+                
+                <p>Receive timely warnings and updates when flood risks are detected, helping you make quick and safe decisions.</p>
+                
+            </div>  
+            
+            <!-- Help Requests -->
+            <div class="features-card">
+                <div class="features-title">
+                    <img width="35" height="35" src="../icons/help_requests.png" alt="Help Requests">
+                    <h4>Help Requests</h4>
+                </div>
+                
+                <p>Send help requests for yourself or others directly to responders or authorities.</p>
+                
+            </div>
+        </div>
+
+    </section>
+
+    <?php require "../views/footer.php" ?>
+    
+</body>
+</html>
---
+++ pages/login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" 
+    integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<body>
+
+    <?php require "../views/index_navbar.php" ?>
+
+    <div class="login-container">
+        <form id="login-form">
+
+            <h2>LOGIN</h2>
+
+            <div class="form-group">
+                <label for="login-email">Email Address</label>
+                <div class="input-box">
+                    <i class="fas fa-envelope"></i>
+                    <input type="email" id="login-email" name="login-email" placeholder="example@gmail.com" required>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="login-password">Password</label>
+                <div class="input-box">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" id="login-password" name="login-password" required>
+                    <i class="fas fa-eye toggle-password" onclick="togglePassword(this)"></i>
+                </div>
+            </div>
+
+            <!-- Error message -->
+            <p id="error-msg"></p>
+
+            <div class="form-options">
+                <div class="remember-me">
+                    <input type="checkbox" id="remember" name="remember">
+                    <label for="remember">Remember me</label>
+                </div>
+                <a onclick="resetPassword()" id="forgot-pass">Forgot Password?</a>
+            </div>
+
+            <div class="form-button">
+                <button type="submit">Login</button>
+            </div>
+
+            <div class="register-text">
+                <p>Don’t have an account?</p>
+                <a href="../pages/sign_up.php">Register Now!</a>
+            </div>
+        </form>
+    </div>
+
+    <?php require "../views/footer.php" ?>
+
+    <script src="../js/script.js"></script>
+</body>
+</html>
