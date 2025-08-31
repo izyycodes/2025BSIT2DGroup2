318085dc6d3e606528c4136880b765c8800a015d
Your Name <you@example.com>
2025-08-31 09:13:23 +0800

    added new_report.css

--- BantayBaha_website/pages/submit_new_report.php
+++ BantayBaha_website/pages/submit_new_report.php
@@ -6,7 +6,7 @@
     <title>New Report</title>
     <link rel="icon" type="image/ico" href="../assets/images/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="../assets/css/style.css"> -->
+    <link rel="stylesheet" href="../assets/css/new_report.css">
     <link rel="stylesheet" href="../assets/css/navbar.css">
     <link rel="stylesheet" href="../assets/css/sidebar.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
@@ -15,6 +15,11 @@
     <?php require "../views/dashboard_navbar.php" ?>
     <?php require "../views/sidebar.php" ?>
 
-    <?php require "../views/footer.php" ?>
+    <div class="main-content">
+        
+        <!-- Put form code here -->
+
+        <?php require "../views/footer.php" ?>
+    </div>
 </body>
 </html>
