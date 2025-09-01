580b06ff0bafc444ee86877b1da1b9b518457bcd
Your Name <you@example.com>
2025-09-01 10:53:11 +0800

    added active in submit new report

--- BantayBaha_website/views/sidebar.php
+++ BantayBaha_website/views/sidebar.php
@@ -28,7 +28,7 @@
                 <span class="tooltip">River Monitoring</span>
             </li>
             <li>
-                <a href="../pages/community_reports.php" class="<?php echo ($current_page == 'community_reports.php') ? 'active' : ''; ?>">
+                <a href="../pages/community_reports.php" class="<?php echo ($current_page == 'community_reports.php' || $current_page == 'submit_new_report.php') ? 'active' : ''; ?>">
                     <i class="fa-solid fa-bullhorn"></i>
                     <span class="links-name">Community Reports</span>
                 </a>
